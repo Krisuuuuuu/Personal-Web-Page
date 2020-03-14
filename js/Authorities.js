938bb77614bc4becb1eca1efb2423f460fa4ea69
Krisuuuuuu <krisu1997@onet.pl>
2020-03-14 18:51:12 +0100

    Added section of contact and footer

--- js/Authorities.js
+++ js/Authorities.js
@@ -2,11 +2,13 @@ const h1 = document.querySelector("section.authorities div.text h1");
 const h2 = document.querySelector("section.authorities div.text h2");
 const grayImage = document.querySelector("section.authorities img.grayImage");
 const colorImage = document.querySelector("section.authorities img.colorImage");
+const background = document.querySelector("section.authorities");
 
 const tabOfQuotations = ["If you don't believe in yourself no one will do it for you", "If a person has no dreams, they no longer have any reason to live"];
 const tabOfAuthorities = ['Kobe Bryant', 'Ayrton Senna'];
 const tabOfGrayImages = ['images/kobe_black.png', 'images/ayrton_black.png'];
 const tabOfColorImages = ['images/kobe_color.png', 'images/ayrton_color.png'];
+const tabOfBgImages = ['C:/Users/krisu/Desktop/Dokumenty/Projekty/Personal%20Web%20Page/images/court.jpg', 'C:/Users/krisu/Desktop/Dokumenty/Projekty/Personal%20Web%20Page/images/yas_marina.jpg'];
 
 let counter = 1;
 let interval = 8000;
@@ -17,6 +19,7 @@ function ChangeAnimationObject()
     h2.textContent = tabOfAuthorities[counter];
     grayImage.src = tabOfGrayImages[counter];
     colorImage.src = tabOfColorImages[counter];
+    background.style.backgroundImage = "url("+tabOfBgImages[counter]+")";
     counter++;
 
     if(counter == 2)
