c983fa25d7de7c8ea8ef4497ab9bca1d6c176d0d
Krisuuuuuu <krisu1997@onet.pl>
2020-03-16 16:37:26 +0100

    Added section about skills

---
+++ js/BarAnimator.js
@@ -0,0 +1,24 @@
+const $spanCSharp = $("section.skills ul.skillsList li:nth-child(1) span.bar span")
+const $spanCPlus = $("section.skills ul.skillsList li:nth-child(2) span.bar span")
+const $spanHtml = $("section.skills ul.skillsList li:nth-child(3) span.bar span")
+const $spanCss = $("section.skills ul.skillsList li:nth-child(4) span.bar span")
+const $spanSql = $("section.skills ul.skillsList li:nth-child(5) span.bar span")
+const $sectionSkills = $("section.skills")
+const sectionSkillsHeight = $sectionSkills.outerHeight();
+const sectionSkillsPosition = $sectionSkills.offset().top;
+
+function ShowBars()
+{
+    let scrollValue = $document.scrollTop();
+
+    if(scrollValue > sectionPosition - windowHeight)
+    {
+        $spanCSharp.addClass("cSharp");
+        $spanCPlus.addClass("cPlus");
+        $spanHtml.addClass("html");
+        $spanCss.addClass("css");
+        $spanSql.addClass("sql");
+    }
+}
+
+$document.on("scroll", ShowBars);
